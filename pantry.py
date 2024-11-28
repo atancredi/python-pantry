099cfb31cd4111448caed330d9f7bbf7054157d5
atancredi <a.tancredi00@gmail.com>
2024-11-28 17:04:30 +0100

    updates, requirement

--- pantry.py
+++ pantry.py
@@ -1,13 +1,17 @@
-import requests, json, os
-import requests
-from math import sqrt, pow
-
+import json, os
+from requests import get, post, put, delete, Response
+from dotenv import load_dotenv
+from os import environ as env
+from dataclasses import dataclass
 
 BASE_URL = "https://getpantry.cloud/apiv1/pantry/"
 HEADERS = {"Content-Type": "application/json"}
 
 
 class Utility:
+    res: Response
+    outputfile: str
+
     # Write json File
     def write_json(self, path, data):
         try:
@@ -31,49 +35,111 @@ class Utility:
     def is_path(self, path):
         return os.path.exists(path)
 
+    def check_response(self):
+        data = None
+        if self.res:
+            try:
+                data = self.res.json()
+            except json.decoder.JSONDecodeError or TypeError:
+                data = self.res.text
 
+        if self.outputfile:
+            self.write_json(path=self.outputfile, data=data)
+
+        return data
+
+@dataclass
+class PantryAccount:
+    name: str
+    description: str
+    errors: list
+    notifications: bool
+    percentFull: float
+    baskets: list
+
+@dataclass
 class Pantry(Utility):
+    account: PantryAccount
+
     def __init__(self, api_key=None) -> None:
-        if api_key:
-            self.api_key = api_key
-        else:
-            raise "Provide an API Key."
+        if not api_key:
+            # Get from .env
+            load_dotenv()
+            api_key = env.get("PANTRY_ID")
+        
+        self.api_key = api_key
+        self.outputfile = None
 
         self.get_account()
 
     def get_account(self):
+        """
+        Delete basket
+        :param basket<str>: basket id
+        :return: text ('... was removed from your Pantry!')
+        """
         url = f"{BASE_URL}{self.api_key}"
-        res = requests.get(url, headers=HEADERS)
-        self.account = res.json()
-        self.baskets = [x["name"] for x in self.account["baskets"]]
+        self.res = get(url, headers=HEADERS)
+        if self.res.status_code != 200:
+            raise ConnectionError(self.res.text)
+        self.account = PantryAccount(**self.res.json())
+        # self.baskets = [x["name"] for x in self.account.baskets]
 
-    def basket(self, basket=None, outputfile=None):
+    def get_basket(self, basket: str = None, outputfile=None):
+        """
+        Delete basket
+        :param basket<str>: basket id
+        :return: text ('... was removed from your Pantry!')
+        """
         self.outputfile = outputfile
         if basket:
             url = f"{BASE_URL}{self.api_key}/basket/{basket}"
-            self.res = requests.get(url, headers=HEADERS)
+            self.res = get(url, headers=HEADERS)
+            if self.res.status_code != 200:
+                raise ConnectionError(self.res.text)
             return self.check_response()
-
+        
     def update(self, basket, data: dict, outputfile=None):
+        """
+        Delete basket
+        :param basket<str>: basket id
+        :return: text ('... was removed from your Pantry!')
+        """
         self.outputfile = outputfile
 
         if not isinstance(data, dict):
             raise TypeError("Data must be a dictionary.")
 
         url = f"{BASE_URL}{self.api_key}/basket/{basket}"
-        res = requests.put(url, headers=HEADERS, json=data)
-        return res
+        self.res = put(url, headers=HEADERS, json=data)
+        if self.res.status_code != 200:
+            raise ConnectionError(self.res.text)
+        return self.check_response()
 
-    def create(self, basket):
+    def create_basket(self, basket: str):
+        """
+        Create a new basket
+        :param basket<str>: basket id
+        :return: text ('Your Pantry was updated with basket: ...')
+        """
         url = f"{BASE_URL}{self.api_key}/basket/{basket}"
-        res = requests.post(url, headers=HEADERS)
-        if res.status_code != 200:
-            raise ConnectionError(res.text)
+        self.res = post(url, headers=HEADERS)
+        if self.res.status_code != 200:
+            raise ConnectionError(self.res.text)
+        self.account.baskets.append()
+        return self.check_response()
 
     def delete(self, basket=None):
+        """
+        Delete basket
+        :param basket<str>: basket id
+        :return: text ('... was removed from your Pantry!')
+        """
         if basket:
             url = f"{BASE_URL}{self.api_key}/basket/{basket}"
-            self.res = requests.delete(url, headers=HEADERS)
+            self.res = delete(url, headers=HEADERS)
+            if self.res.status_code != 200:
+                raise ConnectionError(self.res.text)
             return self.check_response()
 
     def check_response(self):
